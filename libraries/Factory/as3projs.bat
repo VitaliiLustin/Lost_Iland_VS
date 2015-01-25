ec166c121558647df2edbc994ee198296df252b8
antigop <vitalii.lustin@gmail.com>
2015-01-25 19:42:02 +0200

    v

--- libraries/Factory/as3projs.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\FlashDevelop\Tools\fdbuild\fdbuild.exe" "D:\__LUMERIA\Lumeria\libraries\Factory\kitchen\kitchen.as3proj" -compiler "C:\Program Files (x86)\FlashDevelop\Tools\flexsdk" -notrace -library "C:\Program Files (x86)\FlashDevelop\Library";
-java -jar D:\__LUMERIA\ObjectGenerator\bin\reducer.jar -input D:\__LUMERIA\Lumeria\resources\swf\Factory\kitchen.swf -output D:\__LUMERIA\Lumeria\resources\swf\Factory\kitchen.swf -quality 0.6
-
--- libraries/Land/as3projs.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\FlashDevelop\Tools\fdbuild\fdbuild.exe" "D:\__LUMERIA\Lumeria\libraries\Land\land_1\land_1.as3proj" -compiler "C:\Program Files (x86)\FlashDevelop\Tools\flexsdk" -notrace -library "C:\Program Files (x86)\FlashDevelop\Library";
-java -jar D:\__LUMERIA\MapGenerator\MapGenerator\bin\reducer.jar -input D:\__LUMERIA\Lumeria\resources\lands\land_1.swf -output D:\__LUMERIA\Lumeria\resources\lands\land_1.swf -quality 0.6
-
